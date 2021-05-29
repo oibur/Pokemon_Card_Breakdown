8ab562fd47b679005c67075f93f282944f5eaf8f
unknown <bktmcgill@gmail.com>
2021-05-29 13:51:12 -0400

    added graph multiple Pokemon

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-#Libraries required
+#Libraries requried
 import matplotlib.pyplot as plt
 import random
 import requests
@@ -7,9 +7,6 @@ import time
 #My current key for the API
 api_key = 'c2eaa76b-c34c-4d3a-8f33-da95a230d9ea'
 
-#Constant for game function
-N_GAMES = 3
-
 
 def main():
     print('Welcome Pokemon Navigator')
@@ -17,26 +14,31 @@ def main():
     #Starts the master loop
     while True:
         print(f'What would you like to do {name}?')
-        print('Enter 1 to see how many times a Pokemon has appeared on a card')
-        print('Enter 2 to look at the rarity breakdown of a Pokemons cards')
-        print('Enter 3 to play a game of Bulbasaur, Charmander, Squirtle!')
+        print('Enter 1 to see how many cards a Pokemon has appeared on')
+        print("Enter 2 to see the rarity breakdown of a Pokemon's cards")
+        print('Enter 3 to play a game of Bulbasaur Charmander Squirtle!')
         print('Enter 0 to exit')
         #Allows card number look-up through the card function
-        choice = int(input('Choice: '))
+        try:
+            choice = int(input('Choice: '))
+        except (ValueError):
+            print("That is not a valid choice.")
+            break
         if choice == 1:
-            cards()
+            cards_choice()
         #Allows card rarity look-up through the rarity function
         elif choice == 2:
-            rarities()
+            rarity_choice()
         #Allows the user to play a game through the game function
         elif choice == 3:
-            game()
+            game_choice()
         #Breaks the master loop
         else:
+            print("That is not a valid choice.")
             break
 
 
-def make_a_graph(rarity_dict):
+def make_rarity_graph(rarity_dict):
     rarity = []
     number = []
     items = rarity_dict.items()
@@ -46,11 +48,104 @@ def make_a_graph(rarity_dict):
     plt.show()
 
 
+#Returns card rarities for Pokemon in a selected Pokedex range
+def get_rarity(name):
+    a_list = []
+    #Access all cards for an individual Pokemon from API based on Pokedex Number. 
+    endpoint = f'https://api.pokemontcg.io/v2/cards?q=name:{name}'
+    #Returns data on those cards in JSON form
+    data = (requests.get(endpoint, data={'X-api-key': api_key})).json()
+    #For however many cards that Pokemon has been on
+    for i in range(data['count']):
+        #What is the rarity of that card (if exists)
+        try:
+            a_list.append(data['data'][i]['rarity'])
+        #Skips cards that do not include a rarity (i.e. McDonalds promos)
+        except KeyError:
+            pass
+    #Returns a list of card rarities for all Pokemon in the selected range.
+    gen_one_dict = {i:a_list.count(i) for i in a_list}
+    print(gen_one_dict)
+    make_rarity_graph(gen_one_dict)
+
+
+def count_cards(name_of_pokemon):
+    endpoint = f'https://api.pokemontcg.io/v2/cards?q=name:{name_of_pokemon}'
+    data = (requests.get(endpoint, data={'X-api-key': api_key})).json() 
+    count = data['count']
+    if count > 0:
+        print(f'{name_of_pokemon} has appered on {count} cards over the past 25 years.')
+    else:
+        print(f'{name_of_pokemon} is not a valid Pokemon name.')
+    
+
+def count_all_cards(num, card_dict):
+    #sets the API endpath based on the pokemons dex num
+    endpoint = f'https://api.pokemontcg.io/v2/cards?q=nationalPokedexNumbers:{num}'
+    data = (requests.get(endpoint, data={'X-api-key': api_key})).json()
+    #Retrieves data from API and append to dictionary
+    card_dict[data['data'][0]['name']] = data['count']
+    return card_dict
+
+
+gen_one = []
+
+def count_rarities(dex_num, a_list):
+    endpoint = f'https://api.pokemontcg.io/v2/cards?q=nationalPokedexNumbers:{dex_num}'
+    data = (requests.get(endpoint, data={'X-api-key': api_key})).json() 
+    for i in range(data['count']):
+        try:
+            a_list.append(data['data'][i]['rarity'])
+        except KeyError:
+            pass
+    return a_list
+
+
+def rarity_choice():
+    print('You can enter the name of a Pokemon')
+    print("Or enter 'all' to return all Pokemon")
+    print('Warning: all Pokemon will take 5+ min')
+    name = input('Choice: ')
+    if name == 'all':
+        gen_one = []
+        for i in range(1, 894):
+            count_rarities(i+1, gen_one)
+        gen_one_dict = {i:gen_one.count(i) for i in gen_one}
+        print(gen_one_dict)
+        make_rarity_graph(gen_one_dict)
+    else:
+        try:
+            get_rarity(name)
+        except (KeyError):
+            print("That is not a valid response.")
+    time.sleep(2)
+
+def cards_choice():
+    print('You can enter the name of a Pokemon')
+    print("Or enter 'all' to return all Pokemon")
+    print('Warning: all Pokemon will take 5+ min')
+    names = input('Choice: ')
+    if names == 'all':
+        name_counts = dict()
+        for num in range (1, 894):
+            try:
+                count_all_cards(num, name_counts)
+            except IndexError:
+                pass
+        print(name_counts)
+    else:
+        try:
+            count_cards(names)
+        except:
+            print("That is not a valid response.")
+    time.sleep(2)
+
+
 #Function for the rock paper scissors themed game
-def game():
+def game_choice():
     print_welcome()
     score = 0
-    for i in range(N_GAMES):
+    for i in range(3):
         #Decides what the AI will choose
         ai_move = get_ai_move()
         #Gets the human players move
@@ -124,102 +219,13 @@ def get_ai_move():
 def print_welcome():
     print('Welcome to Bulbasaur Charmander Squirtle')
     print('The same as Rock Paper Scissors, but Pokemon themed')
-    print('You will play '+str(N_GAMES)+' games against the AI')
+    print('You will play 3 games against the AI')
     print('Bulbasaur(grass) beats Squirtle(water)')
     print('Squirtle(water) beats Charmander(fire)')
     print('Charmander(fire) beats Bulbasaur(grass')
     print('----------------------------------------------')
     print('')
 
-
-#Returns card rarities for Pokemon in a selected Pokedex range
-def get_rarity(name):
-    a_list = []
-    #Access all cards for an individual Pokemon from API based on Pokedex Number. 
-    endpoint = f'https://api.pokemontcg.io/v2/cards?q=name:{name}'
-    #Returns data on those cards in JSON form
-    data = (requests.get(endpoint, data={'X-api-key': api_key})).json()
-    #For however many cards that Pokemon has been on
-    for i in range(data['count']):
-        #What is the rarity of that card (if exists)
-        try:
-            a_list.append(data['data'][i]['rarity'])
-        #Skips cards that do not include a rarity (i.e. McDonalds promos)
-        except KeyError:
-            pass
-    #Returns a list of card rarities for all Pokemon in the selected range.
-    gen_one_dict = {i:a_list.count(i) for i in a_list}
-    print(gen_one_dict)
-    make_a_graph(gen_one_dict)
-
-
-def get_counts(name_of_pokemon):
-    endpoint = f'https://api.pokemontcg.io/v2/cards?q=name:{name_of_pokemon}'
-    data = (requests.get(endpoint, data={'X-api-key': api_key})).json() 
-    count = data['count']
-    print(f'{name_of_pokemon} has appered on {count} cards over the past 25 years.')
-    
-
-def get_name_count(num, card_dict):
-    #sets the API endpath based on the pokemons dex num
-    endpoint = f'https://api.pokemontcg.io/v2/cards?q=nationalPokedexNumbers:{num}'
-    data = (requests.get(endpoint, data={'X-api-key': api_key})).json()
-    #Retrieves data from API and append to dictionary
-    card_dict[data['data'][0]['name']] = data['count']
-    return card_dict
-
-
-gen_one = []
-
-def get_count(dex_num, a_list):
-    endpoint = f'https://api.pokemontcg.io/v2/cards?q=nationalPokedexNumbers:{dex_num}'
-    data = (requests.get(endpoint, data={'X-api-key': api_key})).json() 
-    for i in range(data['count']):
-        try:
-            a_list.append(data['data'][i]['rarity'])
-        except KeyError:
-            pass
-    return a_list
-
-
-def rarities():
-    print('You can enter the name of a Pokemon')
-    print("Or enter 'all' to return all Pokemon")
-    print('Warning: all Pokemon will take 5+ min')
-    name = input('Choice: ')
-    if name == 'all':
-        gen_one = []
-        for i in range(1, 894):
-            get_count(i+1, gen_one)
-        gen_one_dict = {i:gen_one.count(i) for i in gen_one}
-        print(gen_one_dict)
-        make_a_graph(gen_one_dict)
-    else:
-        try:
-            get_rarity(name)
-        except (KeyError):
-            print("That is not a valid response.")
-    time.sleep(2)
-
-def cards():
-    print('You can enter the name of a Pokemon')
-    print("Or enter 'all' to return all Pokemon")
-    print('Warning: all Pokemon will take 5+ min')
-    names = input('Choice: ')
-    if names == 'all':
-        name_counts = dict()
-        for num in range (1, 894):
-            try:
-                get_name_count(num, name_counts)
-            except IndexError:
-                pass
-        print(name_counts)
-    else:
-        try:
-            get_counts(names)
-        except:
-            print("That is not a valid response.")
-    time.sleep(2)
 
 if __name__ == '__main__':
     main()
